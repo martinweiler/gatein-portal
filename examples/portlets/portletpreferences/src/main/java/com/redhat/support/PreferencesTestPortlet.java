bc1442231a51aa0d7b4b581a6982231810099e0b
Martin Weiler <mweiler@redhat.com>
2015-05-13 10:01:57 +0200

    Working sample, added EDIT mode and doEdit implementation

--- examples/portlets/portletpreferences/src/main/java/com/redhat/support/PreferencesTestPortlet.java
+++ examples/portlets/portletpreferences/src/main/java/com/redhat/support/PreferencesTestPortlet.java
@@ -8,7 +8,6 @@ import javax.portlet.ActionResponse;
 import javax.portlet.GenericPortlet;
 import javax.portlet.PortletException;
 import javax.portlet.PortletPreferences;
-import javax.portlet.PortletSecurityException;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
@@ -25,4 +24,33 @@ public class PreferencesTestPortlet extends GenericPortlet {
         writer.close();
     }
 
+    @Override
+    protected void doEdit(RenderRequest request, RenderResponse response)
+            throws PortletException, IOException {
+        response.setContentType("text/html");
+        PortletPreferences prefs = request.getPreferences();
+        String currentColor = prefs.getValue("color", "");
+        String[] possibleColors = { "black", "blue", "yellow", "green", "red", "orange"};
+        PrintWriter writer = response.getWriter();
+        writer.write("<div>Portlet Preferences</div>");
+        writer.write("<form action='" + response.createActionURL() + "' method='POST'>");
+        writer.write("Select a color: ");
+        writer.write("<select name='color'>");
+        for(String c : possibleColors) {
+            writer.write("<option" + (c.equals(currentColor)?" selected":"") + ">" + c + "</option>");
+        }
+        writer.write("</select>");
+        writer.write("<input type='submit' value='Save'/>");
+        writer.write("</form>");
+        writer.close();
+    }
+
+    @Override
+    public void processAction(ActionRequest request, ActionResponse response)
+            throws PortletException, IOException {
+        String selectedColor = request.getParameter("color");
+        PortletPreferences prefs = request.getPreferences();
+        prefs.setValue("color", selectedColor);
+        prefs.store();
+    }
 }
